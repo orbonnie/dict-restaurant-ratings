f2fbc9261d96bc97fcf3669a461a13a9f87766a9
Bonnie Owens <arlolocos@gmail.com>
2022-11-07 17:23:29 -0500

    refactored to list in descending order

--- ratings.py
+++ ratings.py
@@ -8,11 +8,11 @@ def get_ratings(file_name):
         line = line.split(":")
         ratings[line[0]]=line[1].strip()
 
-    user_restaurant = input("What restaurant would you like to rate?")
-    user_rating = input("On a scale from 1 to 5, what do you rate this restaurant?")
+    user_restaurant = input("What restaurant would you like to rate? ")
+    user_rating = input("On a scale from 1 to 5, what do you rate this restaurant? ")
     ratings[user_restaurant]=user_rating
 
-    ratings=sorted(ratings.items(), key=lambda item: item[1])
+    ratings=sorted(ratings.items(), key=lambda item: item[1], reverse=True)
 
     for ratings in ratings:
         print (f"{ratings[0]} is rated at {ratings[1]}.")
